69ecba8212d7e59b2d9254f067f3e985aa5b240d
Chris Nyhuis <cnyhuis@vigilantnow.com>
2026-01-01 20:06:41 -0500

    Add depends_on to sidecar to ensure unifi starts first
    - Sidecar uses network_mode: container:unifi which requires unifi to exist
    - Without depends_on, sidecar tries to start before unifi causing error
    - Fixes: Service uses the network stack of container which does not exist

--- install.sh
+++ install.sh
@@ -566,6 +566,8 @@ if [ "$IS_VLAN" = "true" ]; then
   unifi-routing-fixer:
     image: alpine:latest
     container_name: unifi-routing-fixer
+    depends_on:
+      - unifi
     network_mode: \"container:unifi\"
     cap_add:
       - NET_ADMIN
